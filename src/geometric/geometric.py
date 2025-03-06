f33163bf4f81be30c67f02f7996e86ecbf0b7979
Jhon <jhonobamagarcia@hotmail.com>
2025-03-05 19:35:04 -0500

    area_trapecio_y_rombo

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -110,7 +110,7 @@ class Geometria:
         Returns:
             float: Área del trapecio
         """
-        pass
+        return ((base_mayor + base_menor) * altura) / 2
     
     def area_rombo(self, diagonal_mayor, diagonal_menor):
         """
@@ -123,7 +123,7 @@ class Geometria:
         Returns:
             float: Área del rombo
         """
-        pass
+        return (diagonal_mayor * diagonal_menor) / 2
     
     def area_pentagono_regular(self, lado, apotema):
         """
