344b32384a36dcd1474e6c0540e80660deb23553
Jhon <jhonobamagarcia@hotmail.com>
2025-03-05 19:55:37 -0500

    volumen_cilindro_y_area_sup_cilindro

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -234,7 +234,7 @@ class Geometria:
         Returns:
             float: Volumen del cilindro
         """
-        pass
+        return math.pi * (radio **2) * altura
     
     def area_superficie_cilindro(self, radio, altura):
         """
@@ -247,7 +247,7 @@ class Geometria:
         Returns:
             float: Área de la superficie del cilindro
         """
-        pass
+        return 2 * math.pi * radio * (radio + altura)
     
     def distancia_entre_puntos(self, x1, y1, x2, y2):
         """
